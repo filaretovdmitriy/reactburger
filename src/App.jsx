27ed91dac2ee558351324bae095906e54ebb71aa
filaretovdmitriy <filaretovd@yandex.ru>
2023-03-11 14:49:49 +0300

    test commit 1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AppHeader from './components/appheader/appheader';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader/>
+      
+
+ 
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/appheader/appheader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function AppHeader(props)
+{
+    return(<></>)
+}
+
+export default AppHeader;
