c983c02f4b9c8cfac0aae956601cce0bb03b04ae
filaretovdmitriy <filaretovd@yandex.ru>
2023-03-11 17:49:54 +0300

    new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import AppHeader from './components/appheader/appheader';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import AppHeader from "./components/appheader/appheader";
+import BurgerConstructor from "./components/burgerconstructor/burgerconstructor";
+import BurgerIngredients from "./components/burgeringredients/burgeringredients";
 
 function App() {
+  const ingredients = null;
+
+  const [state, setState] = useState({
+    ingredients: null,
+    loading: true,
+  });
+
+  useEffect(() => {
+    const getIngredients = async () => {
+      setState({ ...state, loading: true });
+      const res = await fetch(
+        "https://norma.nomoreparties.space/api/ingredients"
+      );
+      const data = await res.json();
+      setState({ ingredients: data.data, loading: false });
+    };
+
+    getIngredients();
+  }, []);
+
   return (
     <div className="App">
-      <AppHeader/>
-      
-
- 
-
+      <AppHeader />
+      <BurgerConstructor />
+      <BurgerIngredients />
+      <div id="react-modals"></div>
     </div>
   );
 }
