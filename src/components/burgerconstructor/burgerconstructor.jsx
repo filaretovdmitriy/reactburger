93020bc46f896a1832cbfe4cf4e9a36a88a67c52
filaretovdmitriy <filaretovd@yandex.ru>
2023-03-11 17:58:54 +0300

    components

---
+++ src/components/burgerconstructor/burgerconstructor.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BurgerConstructor(props) {
+  return <></>;
+}
+
+export default BurgerConstructor;
---
+++ src/components/burgeringredients/burgeringredients.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BurgerIngredients(props) {
+  return <></>;
+}
+
+export default BurgerIngredients;
